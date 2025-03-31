a43d666308a16fb08bcfd4565b9a7e10dcf905ad
Adam Dyess <adam.dyess@canonical.com>
2025-03-31 15:26:19 -0500

    Work towards creating unit tests for a pip package (#4)
    * Work towards creating unit tests for a pip package
    
    * Improved typing

--- charms/contextual_status.py
+++ charms/contextual_status.py
@@ -1,14 +1,21 @@
 import logging
-
+import ops
 from contextlib import contextmanager
-from typing import Optional, Tuple, Type
-from ops import ActiveStatus, BlockedStatus, StatusBase, WaitingStatus
+from typing import List, Optional, Type, TypedDict
 
-contexts = []
 log = logging.getLogger(__name__)
 
 
-def add(status: StatusBase):
+class Context(TypedDict):
+    unit: ops.Unit
+    blocked: List[ops.BlockedStatus]
+    waiting: List[ops.WaitingStatus]
+
+
+contexts: List[Context] = []
+
+
+def add(status: ops.StatusBase):
     """Add unit status to the current context.
 
     If status is MaintenanceStatus, then it is assigned to the unit immediately
@@ -22,16 +29,16 @@ def add(status: StatusBase):
         return
 
     for context in contexts:
-        if type(status) == BlockedStatus:
+        if isinstance(status, ops.BlockedStatus):
             context["blocked"].append(status)
-        elif type(status) == WaitingStatus:
+        elif isinstance(status, ops.WaitingStatus):
             context["waiting"].append(status)
         else:
             context["unit"].status = status
 
 
 @contextmanager
-def context(unit, exit_status: Optional[StatusBase] = None):
+def context(unit: ops.Unit, exit_status: Optional[ops.StatusBase] = None):
     """Create a status context.
 
     Status contexts are used to collect Blocked or Waiting statuses that are
@@ -51,12 +58,12 @@ def context(unit, exit_status: Optional[StatusBase] = None):
         exit_status (StatusBase, optional): The status to set when the exiting
         the context if no other status is set. Defaults to ActiveStatus(Ready)
     """
-    exit_status = exit_status or ActiveStatus("Ready")
+    exit_status = exit_status or ops.ActiveStatus("Ready")
 
     if contexts:
         log.warning("Already in a status context, proceeding anyway")
 
-    context = {"unit": unit, "blocked": [], "waiting": []}
+    context: Context = {"unit": unit, "blocked": [], "waiting": []}
     contexts.append(context)
 
     try:
@@ -82,14 +89,14 @@ class ReconcilerError(Exception):
 
 
 @contextmanager
-def on_error(status: StatusBase, *status_exceptions: Type[Exception]):
+def on_error(status: ops.StatusBase, *status_exceptions: Type[Exception]):
     """Context for emitting status on error.
 
     If an exception occurs within the on_error context body, then add the
     specified status to the status context. This can be used as a function
     decorator to emit Blocked or Waiting status on error with less try/except
     boilerplate.
-    
+
     By default, on_error catches all exceptions, but it can be tuned to only
     catch specific exception types passed in status_exceptions.  When tuned,
     the status is only added to the context on the passed exceptions, and other
